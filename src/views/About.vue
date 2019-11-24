3b7798db4fcc2b49f09f13beb79357abcbab5692
qiugu <476710564@qq.com>
2019-11-24 17:27:52 +0800

    styles: code styles

--- src/views/About.vue
+++ src/views/About.vue
@@ -20,6 +20,7 @@ export default {
   data() {
     return {
       floatingBodyClass: {
+        /* eslint-disable */
         backgroundImage: `url(${require('../assets/floating-bg.jpg')})`,
         backgroundSize: 'cover',
       },
