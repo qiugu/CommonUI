f09d438a907f9379a889bc0d820850b707fbfd08
qiugu <476710564@qq.com>
2019-11-24 17:39:20 +0800

    feat: init screen component

---
+++ packages/screen/ConditionScreen.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="screen">
+    <div v-for="(item, index) in searchData" :key="index" class="screen-type">
+      <strong :style="{ marginRight: '20px' }">{{ item.name }}</strong>
+      <template v-for=" tag in item.tags">
+        <label :key="tag.value">
+          <input
+            :value="tag.value"
+            :key="tag.value"
+            :type="type"
+            :name="tag.type"
+            @change="(checked) => handleChange(tag, checked)"
+          />
+          <span :class="{ 'is-checked': isChecked }">{{ tag.label }}</span>
+        </label>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
